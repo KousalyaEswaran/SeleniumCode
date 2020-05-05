169f6cff49b08238cfd78312fb0d932f5f9b9a4a
KousalyaEswaran <64738511+KousalyaEswaran@users.noreply.github.com>
2020-05-05 16:16:23 +0530

    updated

--- week1/day1/classroom/week1/day1/classroom/SwitchOperator.java
+++ week1/day1/classroom/week1/day1/classroom/SwitchOperator.java
@@ -5,48 +5,64 @@ public class SwitchOperator {
 	/*
 	 * Goal: Perform calculation based on the user input operations
 	 * 
-	 * inputs: 2,3 and "add"
-	 * output: added value: 5
+	 * inputs: 2,3 and "add" output: added value: 5
 	 * 
-	 * Shortcuts:
-	 * 1) Print : type: syso, followed by: ctrl + space + enter
-	 * 2) To create a Switch case: type 'switch', followed by: ctrl + space + down arrow + enter
-	 *   
-	 * What are my learnings from this code?
-	 * 1)
-	 * 2)
-	 * 3) 
+	 * Shortcuts: 1) Print : type: syso, followed by: ctrl + space + enter 2) To
+	 * create a Switch case: type 'switch', followed by: ctrl + space + down arrow +
+	 * enter
 	 * 
-	 */	
-	
+	 * What are my learnings from this code? 1) 2) 3)
+	 * 
+	 */
+
 	public static void main(String[] args) {
-		
+
 		// Declare 2 integer numbers
-		
 
 		// Declare a String variable with input as operations (Tip:
-		
 
 		// Initiate switch case for operations
-		
-		
-			// Within switch, include as case for add operation
-		
-		
-			// Within switch, include as case for sub operation
-		
-		
-			// Within switch, include as case for mul operation
-		
-		
-			// Within switch, include as case for div operation
-		
-		
-			// Within switch, include 'default' to handle other operations
-		
-		
-		//end of switch case
-		
+
+		// Within switch, include as case for add operation
+
+		// Within switch, include as case for sub operation
+
+		// Within switch, include as case for mul operation
+
+		// Within switch, include as case for div operation
+
+		// Within switch, include 'default' to handle other operations
+
+		// end of switch case
+
+		int input1 = 20;
+		int input2 = 10;
+		int result = 0;
+		String s = "add";
+
+		switch (s) {
+		case "add":
+			result = input1 + input2;
+			System.out.println(result);
+			break;
+		case "sub":
+			result = input1 - input2;
+			System.out.println(result);
+			break;
+		case "mul":
+			result = input1 * input2;
+			System.out.println(result);
+			break;
+		case "div":
+			result = input1 / input2;
+			System.out.println(result);
+			break;
+
+		default:
+			System.out.println("wrong operator");
+			break;
+		}
+
 	}
 
 }
